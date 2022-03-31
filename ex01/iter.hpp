822780469d11f46d7f103bf5ff7736c02852aca2
Mohamed Haddi <m.haddi.95@gmail.com>
2022-03-31 01:56:25 +0000

    change pointer to function

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,24 +6,26 @@
 /*   By: mhaddi <mhaddi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 23:16:46 by mhaddi            #+#    #+#             */
-/*   Updated: 2022/03/31 00:00:29 by mhaddi           ###   ########.fr       */
+/*   Updated: 2022/03/31 01:54:07 by mhaddi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <iostream>
+
 template<typename T>
-void iter(T* arr, int size, void(*f)(T*))
+void iter(T* arr, int size, void(*f)(T const &))
 {
 	for (int i = 0; i < size; i++)
-		f(&arr[i]);
+		f(arr[i]);
 }
 
 template<typename T>
-void dup(T* x)
+void printDup(T const & x)
 {
-	*x += *x;
+	std::cout << x+x << std::endl;
 }
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,12 +6,13 @@
 /*   By: mhaddi <mhaddi@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 23:13:20 by mhaddi            #+#    #+#             */
-/*   Updated: 2022/03/30 23:53:31 by mhaddi           ###   ########.fr       */
+/*   Updated: 2022/03/31 01:54:50 by mhaddi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 #include <iostream>
+
 #define SIZE 3
 
 int main(void) {
@@ -29,18 +30,8 @@ int main(void) {
 		std::cout << arr2[i] << " ";
 	std::cout << std::endl;
 
-	::iter<int>(arr1, SIZE, &dup<int>);
-	::iter<std::string>(arr2, SIZE, &dup<std::string>);
-
-	std::cout << "arr1: ";
-	for (int i = 0; i < SIZE; i++)
-		std::cout << arr1[i] << " ";
-	std::cout << std::endl;
-	
-	std::cout << "arr2: ";
-	for (int i = 0; i < SIZE; i++)
-		std::cout << arr2[i] << " ";
-	std::cout << std::endl;
+	::iter<int>(arr1, SIZE, printDup<int>);
+	::iter<std::string>(arr2, SIZE, printDup<std::string>);
 
 	return 0;
 
